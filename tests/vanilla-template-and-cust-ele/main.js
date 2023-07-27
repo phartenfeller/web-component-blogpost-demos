acc16b3125708c4a60ab7d05fb579f790dd4632b
phartenfeller <pharti@hotmail.de>
2023-07-27 21:20:53 +0200

    initial commit

---
+++ tests/vanilla-template-and-cust-ele/main.js
@@ -0,0 +1,58 @@
+const template = document.createElement("template");
+template.id = "pool-calculator-template";
+template.innerHTML = `
+<style>
+.dimension-inputs {
+  display: grid;
+  grid-template-columns: 15ch 10ch;
+  row-gap: 0.4rem;
+}
+
+#calculate {
+  margin: 0.8rem 0;
+  padding: 0.3rem;
+  background-color: #7dd3fc;
+  border: 1px solid #7dd3fc;
+  border-radius: 0.25rem;
+  color: #0c4a6e;
+}
+
+#calculate:hover {
+  background-color: #e0f2fe;
+}
+
+#calculate:focus {
+  border-color: #0c4a6e;
+}
+
+</style>
+<div class="input-section">
+  <div class="dimension-inputs">
+    <label for="length">Length (meters)</label>
+    <input id="length" type="number" min="0" />
+    <label for="width">Width (meters)</label>
+    <input id="width" type="number" min="0" />
+    <label for="depth">Depth (meters)</label>
+    <input id="depth" type="number" min="0" />
+  </div>
+  <button id="calculate">Calculate</button>
+</div>
+<div class="output-section">
+  <label>Water Capacity (cubic liters): </label>
+  <span id="capacity"></span>
+  <br />
+  <label>Surface Area (square meters): </label>
+  <span id="area"></span>
+</div>
+`;
+
+class PoolDimensions extends HTMLElement {
+  constructor() {
+    // always call super() first in the constructor
+    super();
+    const templateClone = template.content.cloneNode(true);
+    this.appendChild(templateClone);
+  }
+}
+
+customElements.define("pool-dimensions", PoolDimensions);
---
+++ vanilla-js/main.js
@@ -0,0 +1,127 @@
+const template = document.createElement("template");
+template.id = "pool-calculator-template";
+template.innerHTML = `
+<style>
+.dimension-inputs {
+  display: grid;
+  grid-template-columns: 15ch 10ch;
+  row-gap: 0.4rem;
+}
+
+#calculate {
+  margin: 0.8rem 0;
+  padding: 0.3rem;
+  background-color: #7dd3fc;
+  border: 1px solid #7dd3fc;
+  border-radius: 0.25rem;
+  color: #0c4a6e;
+}
+
+#calculate:hover {
+  background-color: #e0f2fe;
+}
+
+#calculate:focus {
+  border-color: #0c4a6e;
+}
+
+</style>
+<div class="input-section">
+  <div class="dimension-inputs">
+    <label for="length">Length (<span class="length-unit">meters</span>)</label>
+    <input id="length" type="number" min="0">
+    <label for="width">Width (<span class="length-unit">meters</span>)</label>
+    <input id="width" type="number" min="0">
+    <label for="depth">Depth (<span class="length-unit">meters</span>)</label>
+    <input id="depth" type="number" min="0">
+  </div>
+  <button id="calculate">Calculate</button>
+</div>
+<div class="output-section">
+  <label>Water Capacity (<span class="capacity-unit">cubic liters</span>): </label>
+  <span id="capacity"></span>
+  <br>
+  <label>Surface Area (<span class="area-unit">square meters</span>): </label>
+  <span id="area"></span>
+</div>
+`;
+
+class PoolDimensions extends HTMLElement {
+  static get observedAttributes() {
+    return ["units"];
+  }
+
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+    this.lengthInput = this.shadowRoot.getElementById("length");
+    this.widthInput = this.shadowRoot.getElementById("width");
+    this.depthInput = this.shadowRoot.getElementById("depth");
+    this.capacityOutput = this.shadowRoot.getElementById("capacity");
+    this.areaOutput = this.shadowRoot.getElementById("area");
+
+    this.lengthUnits = this.shadowRoot.querySelectorAll(".length-unit");
+    this.capacityUnits = this.shadowRoot.querySelector(".capacity-unit");
+    this.areaUnits = this.shadowRoot.querySelector(".area-unit");
+
+    this.calculate = this.calculate.bind(this);
+
+    this.isMetric = true;
+  }
+
+  connectedCallback() {
+    this.shadowRoot
+      .getElementById("calculate")
+      .addEventListener("click", this.calculate);
+    this.setUnits();
+  }
+
+  disconnectedCallback() {
+    this.shadowRoot
+      .getElementById("calculate")
+      .removeEventListener("click", this.calculate);
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === "units") {
+      this.setUnits();
+    }
+  }
+
+  setUnits() {
+    const units = this.getAttribute("units") ?? "metric";
+    this.isMetric = units === "metric";
+
+    this.lengthUnits.forEach(
+      (el) => (el.textContent = this.isMetric ? "meters" : "feet")
+    );
+    this.capacityUnits.textContent = this.isMetric ? "cubic liters" : "gallons";
+    this.areaUnits.textContent = this.isMetric
+      ? "square meters"
+      : "square feet";
+  }
+
+  calculate() {
+    let length = this.lengthInput.valueAsNumber;
+    let width = this.widthInput.valueAsNumber;
+    let depth = this.depthInput.valueAsNumber;
+
+    const capacity = length * width * depth;
+    const area = 2 * (width * depth + length * depth) + length * width; // walls + floor
+
+    if (!this.isMetric) {
+      // Convert cubic meters to gallons
+      this.capacityOutput.textContent = isNaN(capacity)
+        ? ""
+        : (capacity * 264.172).toFixed(1);
+    } else {
+      this.capacityOutput.textContent = isNaN(capacity) ? "" : capacity;
+    }
+    this.areaOutput.textContent = isNaN(area) ? "" : area;
+  }
+}
+
+customElements.define("pool-dimensions", PoolDimensions);
